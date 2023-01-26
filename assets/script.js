a648e7ef6d2094ebae25f5046b37bcee7ed5e5c9
Jean-François UNG <jeffromthere@protonmail.com>
2023-01-26 21:43:33 +0100

    maj

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,7 @@ const slides = [
 	{
 		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+	},
 ]; 
 
 let position = 0;
@@ -23,37 +23,51 @@ const arrowLeft = document.querySelector('.arrow_left');
 const arrowRight = document.querySelector('.arrow_right');
 const image = document.querySelector('.image_js');
 const tagLine = document.querySelector('.text_js');
-const dots = document.querySelectorAll('.dots .dot');
+const dots = [];
+const longueur = slides.length;
+const dotsParent = document.querySelector('.dots');
+
+
+for (let i = 0; i < longueur; i++) {
+	createDot();
+}
+  
+function createDot() {
+	const dot = document.createElement('div');
+	dot.classList.add('dot');
+	dots.push(dot);
+	dotsParent.appendChild(dot);
+}
 
 arrowLeft.addEventListener('click', function() {
 	position = position-1;
 	
 	if (position<0) {
-		position = 3;
+		position = longueur-1;
 	}
-	
-	image.src = slides[position].image;
-	tagLine.innerHTML = slides[position].tagLine;
-	resetDotClass();
-	dots[position].classList.add('dot_selected');
-	
+	changeImageTaglineDot()
 });
 
 arrowRight.addEventListener('click', function() {
 	position = position+1;
 
-	if (position>3) {
+	if (position>(longueur-1)) {
 		position = 0;
 	}
+	changeImageTaglineDot()
+});
 
+function changeImageTaglineDot() {
 	image.src = slides[position].image;
 	tagLine.innerHTML = slides[position].tagLine;
 	resetDotClass();
 	dots[position].classList.add('dot_selected');
-});
+	
+}
 
 function resetDotClass() {
 	dots.forEach ((dot) => {
   		dot.classList.remove('dot_selected');
 	});
 };
+
